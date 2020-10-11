376a85877650ecba369a034a4799913357314dfa
pabloesperi <pabloesperi@hotmail.com>
2020-10-11 11:51:47 +0200

    second commit

---
+++ src/main/java/com/proyects/microservices/app/oauthmicroservice/OauthMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.proyects.microservices.app.oauthmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyects/microservices/app/oauthmicroservice/OauthMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyects.microservices.app.oauthmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
